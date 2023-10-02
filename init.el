828be4b6bcce66cd852baf2ed9b0c2173ea93e1b
Rowan Pellow-Jarman <rowanpellow@gmail.com>
2023-10-02 08:54:50 +0200

    Switch from lsp-mode for python development to anaconda-mode.
    Anaconda-mode has better remote editing support.

--- init.el
+++ init.el
@@ -8,13 +8,13 @@
 (use-package company :ensure t)
 (use-package company-auctex :ensure t)
 (use-package company-math :ensure t)
+(use-package company-anaconda :ensure t)
 (use-package hydra :ensure t)
 (use-package ivy :ensure t)
 (use-package ivy-hydra :ensure t)
 (use-package latex-extra :ensure t)
 (use-package latex-preview-pane :ensure t)
 (use-package lsp-mode :ensure t)
-(use-package lsp-python-ms :ensure t)
 (use-package lsp-latex :ensure t)
 (use-package magit :ensure t)
 (use-package pdf-tools :ensure t)
@@ -25,7 +25,7 @@
 (use-package ssh :ensure t)
 (use-package swiper :ensure t)
 (use-package yasnippet :ensure t)
-
+(use-package anaconda-mode :ensure t)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -44,7 +44,7 @@
  '(custom-safe-themes
    '("51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "524fa911b70d6b94d71585c9f0c5966fe85fb3a9ddd635362bfabd1a7981a307" "efcecf09905ff85a7c80025551c657299a4d18c5fcfedd3b2f2b6287e4edd659" "57a29645c35ae5ce1660d5987d3da5869b048477a7801ce7ab57bfb25ce12d3e" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default))
  '(package-selected-packages
-   '(pyvenv lsp-python-ms reveal-in-folder ivy-hydra idomenu hydra avy swiper ssh company-auctex resize-window lsp-latex company-math latex-preview-pane latex-extra pdf-tools slime-company company ein solarized-theme magit slime))
+   '(company-anaconda anaconda-mode pyvenv lsp-python-ms reveal-in-folder ivy-hydra idomenu hydra avy swiper ssh company-auctex resize-window lsp-latex company-math latex-preview-pane latex-extra pdf-tools slime-company company ein solarized-theme magit slime))
  '(pdf-view-incompatible-modes
    '(linum-mode linum-relative-mode helm-linum-relative-mode nlinum-mode nlinum-hl-mode nlinum-relative-mode yalinum-mode display-line-numbers-mode))
  '(tramp-remote-path
@@ -131,19 +131,20 @@
 (autoload 'idomenu "idomenu" nil t)
 
 ;; add remote python lsp client for tramp
-(lsp-register-client
- (make-lsp-client :new-connection (lsp-tramp-connection "pyls")
-                  :major-modes '(python-mode)
-                  :remote? t
-                  :server-id 'pyls-remote))
+;; (lsp-register-client
+;;  (make-lsp-client :new-connection (lsp-tramp-connection "pyls")
+;;                   :major-modes '(python-mode)
+;;                   :remote? t
+;;                   :server-id 'pyls-remote))
 
-;; add lsp hook to python mode
-(add-hook 'python-mode-hook 'lsp)
+;; add anaconda hook to python mode
+(add-hook 'python-mode-hook 'anaconda-mode)
+(add-hook 'anaconda-mode 'anaconda-eldoc-mode)
 
 (setq mac-option-modifier 'meta
       mac-command-modifier 'meta)
 
-(setq tramp-default-remote-shell "/bin/bash")
+(setq-default tramp-default-remote-shell "/bin/bash")
 
 (use-package pyvenv
   :ensure t
