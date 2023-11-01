9db8155570b42d672bf78d66568baeae99340c38
Rowan Pellow-Jarman <rowanpellow@gmail.com>
2023-11-01 12:58:59 +0200

    Use lsp-mode and pyright language server instead of anaconda mode for editing python files.

--- config/python-config.el
+++ config/python-config.el
@@ -2,6 +2,8 @@
 (use-package anaconda-mode :ensure t)
 (use-package company-anaconda :ensure t)
 
+(use-package lsp-pyright :ensure t)
+
 (require 'dap-python)
 (setq dap-python-debugger 'debugpy)
 
@@ -20,6 +22,7 @@
 
 (use-package flymake-python-pyflakes :ensure t)
 
+(require 'lsp-pyright)
 ;; add remote python lsp client for tramp
 ;; (lsp-register-client
 ;;  (make-lsp-client :new-connection (lsp-tramp-connection "pyls")
@@ -28,14 +31,13 @@
 ;;                   :server-id 'pyls-remote))
 
 ;; configure python development environment
-(add-hook 'python-mode-hook 'anaconda-mode)
-(add-hook 'anaconda-mode 'anaconda-eldoc-mode)
-(eval-after-load "company"
-  '(add-to-list 'company-backends 'company-anaconda))
+;; (add-hook 'python-mode-hook 'anaconda-mode)
+;; (add-hook 'anaconda-mode 'anaconda-eldoc-mode)
+;; (eval-after-load "company"
+;;   '(add-to-list 'company-backends 'company-anaconda))
 
 ;; configure python flymake integration (requires installing pyflakes
 ;; executable)
-
 (setq flymake-python-pyflakes-executable (cl-case system-type
 					   (gnu/linux "pyflakes3")
 					   (darwin "pyflakes")
@@ -43,5 +45,6 @@
 (add-hook 'python-mode-hook 'flymake-mode)
 (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
 (add-hook 'python-mode-hook 'dap-mode)
+(add-hook 'python-mode-hook 'lsp)
 
 (provide 'python-config)
