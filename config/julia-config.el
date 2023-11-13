3cf49811b67b314a82b132d9e265e4f2299502ec
Rowan Pellow-Jarman <rowanpellow@gmail.com>
2023-11-13 06:54:41 +0200

    Install and configure Julia REPL.

--- config/julia-config.el
+++ config/julia-config.el
@@ -1,8 +1,10 @@
 (require 'use-package)
 (use-package lsp-mode :ensure t)
 (use-package lsp-julia :ensure t)
+(use-package julia-repl :ensure t)
 
-(add-hook 'julia-mode-hook #'lsp-mode)
-(add-hook 'julia-mode-hook #'lsp)
+(add-hook 'julia-mode-hook 'lsp-mode)
+(add-hook 'julia-mode-hook 'lsp)
+(add-hook 'julia-mode-hook 'julia-repl-mode)
 
 (provide 'julia-config)
