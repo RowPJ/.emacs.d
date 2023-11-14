dd4442e4e1d7109fcf28e2eae77eb69101d4a83c
Rowan Pellow-Jarman <rowanpellow@gmail.com>
2023-11-14 06:50:22 +0200

    Use julia-snail on platforms where the dependencies can be easily installed, use the previous solution as a fallback.

--- config/julia-config.el
+++ config/julia-config.el
@@ -1,10 +1,19 @@
 (require 'use-package)
-(use-package lsp-mode :ensure t)
-(use-package lsp-julia :ensure t)
-(use-package julia-repl :ensure t)
 
-(add-hook 'julia-mode-hook 'lsp-mode)
-(add-hook 'julia-mode-hook 'lsp)
-(add-hook 'julia-mode-hook 'julia-repl-mode)
+(cl-case system-type
+  ;; On linux / macos, use julia-snail since we can install
+  ;; dependencies easily. On ubuntu 20.04, the dependency names are
+  ;; libvterm-bin and libtool-bin and they can be installed with apt.
+  ((gnu/linux darwin)
+   (use-package julia-snail :ensure t)
+   (add-hook 'julia-mode-hook 'julia-snail-mode))
+  ;; on windows / unknown, fall back to a combination of other modes
+  (t
+   (use-package lsp-mode :ensure t)
+   (use-package lsp-julia :ensure t)
+   (use-package julia-repl :ensure t)
+   (add-hook 'julia-mode-hook 'lsp-mode)
+   (add-hook 'julia-mode-hook 'lsp)
+   (add-hook 'julia-mode-hook 'julia-repl-mode)))
 
 (provide 'julia-config)
