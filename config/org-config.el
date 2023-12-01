c2c5e9823726c86a706d1297bc7d897bb8890e1d
Rowan Pellow-Jarman <rowanpellow@gmail.com>
2023-12-01 06:57:57 +0200

    Enabled a bunch of languages for source blocks in org config.

--- config/org-config.el
+++ config/org-config.el
@@ -1,7 +1,35 @@
 ;; enable executing python code blocks in org mode
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((python . t)))
+ '((python . t)
+;   (sh t)
+;   (bash t)
+   (R t)
+   (awk t)
+;   (zsh t)
+   (shell t)
+   (scheme t)
+   (org t)
+   (lua t)
+   (makefile t)
+   (matlab t)
+   (maxima t)
+   (latex t)
+   (julia t)
+   (haskell t)
+   (forth t)
+   (fortran t)
+   (gnuplot t)
+   (eshell t)
+   (emacs-lisp t)
+;   (elisp t)
+;   (csh t)
+   (calc t)
+   (clojure t)
+
+;   (C++ t)
+;   (cpp t)
+   (C t)))
 
 ;; display line numbers in org-mode. we should maybe turn this off
 ;; because it could be confusing when text is folded.
