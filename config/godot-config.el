64c18394e12cd68391fb546587aed57afa1c45f6
Rowan Pellow-Jarman <rowanpellow@gmail.com>
2026-02-09 16:14:49 +0200

    Activate lsp mode with gdscript mode.

--- config/godot-config.el
+++ config/godot-config.el
@@ -2,4 +2,8 @@
 
 (use-package gdscript-mode :ensure t)
 
+(add-hook 'gdscript-mode-hook 'lsp)
+(when (eql system-type 'windows-nt)
+  (setq gdscript-docs-local-path "C:/Godot/godot-docs-html-stable"))
+
 (provide 'godot-config)
